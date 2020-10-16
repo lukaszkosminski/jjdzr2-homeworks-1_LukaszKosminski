5189fca9888227e0d18fc9c8d38b57da6e5a4aa2
Maciej Kucharski <maciekek@gmail.com>
2020-10-16 09:33:18 +0200

    Update Calc.java

--- Ex1/src/Calc.java
+++ Ex1/src/Calc.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Calc {
 
     public static void main(String[] args) {
-
         System.out.println("enter first number: ");
+
         Scanner scanner = new Scanner(System.in);
         double value1 = scanner.nextInt();
+        
         System.out.println("enter second number: ");
         double value2 = scanner.nextInt();
+        
         System.out.println("enter a math symbol ( +, -, *, / ): ");
         String symbol = scanner.next();
 
@@ -18,25 +20,31 @@ public class Calc {
                 System.out.println("result: " + result);
                 break;
             }
+                
             case "-": {
                 double result = value1 - value2;
                 System.out.println("result: " + result);
                 break;
             }
+                
             case "*": {
                 double result = value1 * value2;
                 System.out.println("result: " + result);
                 break;
             }
+                
             case "/": {
                 if(value2 ==0){
                     System.out.println("don't divide by zero");
                     break;
                 }
+                
                 double result = value1 / value2;
+                
                 System.out.println("result: " + result);
                 break;
             }
+                
             default: {
                 System.out.println("invalid math symbol!");
             }
