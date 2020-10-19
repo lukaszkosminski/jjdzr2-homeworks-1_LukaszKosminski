f8cea3558341d4d20114c2e70ffd3e4f13bc6038
lukaszkosminski <dnb8@o2.pl>
2020-10-19 14:48:16 +0200

    add if in main class

--- Ex1/src/CalcExtra.java
+++ Ex1/src/CalcExtra.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CalcExtra {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("enter first number: ");
         Scanner scanner = new Scanner(System.in);
         double value1 = scanner.nextInt();
@@ -10,36 +10,29 @@ public class CalcExtra {
         double value2 = scanner.nextInt();
         System.out.println("enter a math symbol ( +, -, *, / ): ");
         String symbol = scanner.next();
-        double wynik =calc(value1, value2, symbol);
-        System.out.println("Wynik : " + wynik);
+        if (value2 == 0 && symbol.equals("/")) {
+            System.out.println("don't divide by zero");
+        }else if(!symbol.equals("+") && !symbol.equals("-") && !symbol.equals("/") && !symbol.equals("*")){
+            System.out.println("invalid math symbol!");
+        }else{
+            double wynik =calc(value1, value2, symbol);
+            System.out.println("Wynik : " + wynik);
+        }
     }
     public static double calc(double value1, double value2, String symbol){
         double result=0;
         switch (symbol) {
             case "+": {
-                result = value1 + value2;
-                break;
+                return  value1 + value2;
             }
             case "-": {
-                result = value1 - value2;
-                break;
+                return  value1 - value2;
             }
             case "*": {
-                result = value1 * value2;
-                break;
+                return value1 * value2;
             }
-            //zle dziala, przy dzieleniu przez 0 metoda zwraca wynik
             case "/": {
-                if (value2 == 0) {
-                    System.out.println("don't divide by zero, podaj prawidlową wartość");
-                }else{
-                result = value1 / value2;
-                break;
-                }
-            }
-            //zle działa, przy invalid symbol zwraca wynik
-            default: {
-                System.out.println("invalid math symbol!");
+                return value1 / value2;
             }
         }
         return result;
